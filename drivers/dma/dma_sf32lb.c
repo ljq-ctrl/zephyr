5ab69df3237be09ad283892b1f6b58c1f3d20ee8
jiqunli <jiqunli@sifli.com>
2025-11-01 18:38:46 +0800

    drivers: audio: sf32lb52x: audio codec driver #97906
    audio playback and capure, extend API in codec.h
    
    Signed-off-by: ljq-ctrl <jiqunli@sifli.com>

--- drivers/dma/dma_sf32lb.c
+++ drivers/dma/dma_sf32lb.c
@@ -44,6 +44,7 @@ LOG_MODULE_REGISTER(dma_sf32lb, CONFIG_DMA_LOG_LEVEL);
 #define DMAC_CSELR2    offsetof(DMAC_TypeDef, CSELR2)
 
 #define DMAC_ISR_TCIF(n) (DMAC_ISR_TCIF1_Msk << (n * 4U))
+#define DMAC_ISR_HTIF(n) (DMAC_ISR_HTIF1_Msk << (n * 4U))
 
 #define DMAC_IFCR_ALL(n)                                                                           \
 	((DMAC_IFCR_CGIF1_Msk | DMAC_IFCR_CTCIF1_Msk | DMAC_IFCR_CHTIF1_Msk |                      \
@@ -51,6 +52,7 @@ LOG_MODULE_REGISTER(dma_sf32lb, CONFIG_DMA_LOG_LEVEL);
 	 << (n * 4U))
 #define DMAC_IFCR_CTCIF(n) (DMAC_IFCR_CTCIF1_Msk << (n * 4U))
 #define DMAC_IFCR_CTEIF(n) (DMAC_IFCR_CTEIF1_Msk << (n * 4U))
+#define DMAC_IFCR_CTHIF(n) (DMAC_IFCR_CHTIF1_Msk << (n * 4U))
 
 #define DMAC_CCRX_PSIZE(n) FIELD_PREP(DMAC_CCR1_PSIZE_Msk, LOG2CEIL(n))
 #define DMAC_CCRX_MSIZE(n) FIELD_PREP(DMAC_CCR1_MSIZE_Msk, LOG2CEIL(n))
@@ -72,6 +74,7 @@ struct dma_sf32lb_config {
 struct dma_sf32lb_channel {
 	dma_callback_t callback;
 	void *user_data;
+	uint32_t size;
 	enum dma_channel_direction direction;
 };
 
@@ -89,6 +92,8 @@ static void dma_sf32lb_isr(const struct device *dev, uint8_t channel)
 	isr = sys_read32(config->dmac + DMAC_ISR);
 	if ((isr & DMAC_ISR_TCIF(channel)) != 0U) {
 		status = DMA_STATUS_COMPLETE;
+	} else if ((isr & DMAC_ISR_HTIF(channel)) != 0U) {
+		status = DMA_STATUS_BLOCK;
 	} else {
 		status = -EIO;
 	}
@@ -167,6 +172,11 @@ static int dma_sf32lb_config(const struct device *dev, uint32_t channel,
 		return -EINVAL;
 	}
 
+	if (config_dma->dest_data_size != config_dma->source_data_size) {
+		LOG_ERR("dest_data_size != source_data_size not support");
+		return -EINVAL;
+	}
+
 	/* configure transfer parameters */
 	ccrx = sys_read32(config->dmac + DMAC_CCRX(channel));
 	if ((ccrx & DMAC_CCR1_EN) != 0U) {
@@ -180,6 +190,14 @@ static int dma_sf32lb_config(const struct device *dev, uint32_t channel,
 		  DMAC_CCR1_MEM2MEM_Msk);
 
 	ccrx |= FIELD_PREP(DMAC_CCR1_PL_Msk, config_dma->channel_priority);
+
+	if (config_dma->head_block->dest_reload_en || config_dma->head_block->source_reload_en) {
+		ccrx |= DMAC_CCR1_CIRC;
+	}
+
+	if (config_dma->complete_callback_en) {
+		ccrx |= DMAC_CCR1_HTIE;
+	}
 
 	switch (config_dma->channel_direction) {
 	case MEMORY_TO_MEMORY:
@@ -247,6 +265,7 @@ static int dma_sf32lb_config(const struct device *dev, uint32_t channel,
 	config->channels[channel].callback = config_dma->dma_callback;
 	config->channels[channel].user_data = config_dma->user_data;
 	config->channels[channel].direction = config_dma->channel_direction;
+	config->channels[channel].size = config_dma->source_data_size;
 
 	return 0;
 }
@@ -277,7 +296,7 @@ static int dma_sf32lb_reload(const struct device *dev, uint32_t channel, uint32_
 	}
 
 	/* configure size, src/dst addresses */
-	sys_write32(size, config->dmac + DMAC_CNDTRX(channel));
+	sys_write32(size / config->channels[channel].size, config->dmac + DMAC_CNDTRX(channel));
 
 	switch (config->channels[channel].direction) {
 	case MEMORY_TO_MEMORY:
@@ -313,6 +332,7 @@ static int dma_sf32lb_start(const struct device *dev, uint32_t channel)
 
 	ccrx = sys_read32(config->dmac + DMAC_CCRX(channel));
 	if ((ccrx & DMAC_CCR1_EN) != 0U) {
+		LOG_ERR("start not possible with DMA enabled");
 		return 0;
 	}
 
